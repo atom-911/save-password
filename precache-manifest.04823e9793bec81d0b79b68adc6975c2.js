24b335f68e214aae2982c3cd6ca7fa2ec9096b75
Trung Nguyen <atom.911@hotmail.com>
2019-10-22 20:30:58 +0700

    Updates

---
+++ precache-manifest.04823e9793bec81d0b79b68adc6975c2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b3cd05be3a43b6f4b231620fc867d4a",
+    "url": "/save-password/index.html"
+  },
+  {
+    "revision": "453cc74c226b9213af42",
+    "url": "/save-password/static/css/main.9f639a70.chunk.css"
+  },
+  {
+    "revision": "c0c97a09f1b47d6be009",
+    "url": "/save-password/static/js/2.cc0b5a87.chunk.js"
+  },
+  {
+    "revision": "453cc74c226b9213af42",
+    "url": "/save-password/static/js/main.8d5389df.chunk.js"
+  },
+  {
+    "revision": "eca426268be18c5fa2d9",
+    "url": "/save-password/static/js/runtime~main.cc33b4f7.js"
+  }
+]);
